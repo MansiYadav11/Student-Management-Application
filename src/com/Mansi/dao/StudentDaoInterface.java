13dbfe0bca3a98da5ba1086143fac3052b410048
Mansi Yadav <mansiydv1111@gmail.com>
2026-04-18 21:42:02 +0530

    Update StudentDaoInterface.java

--- src/com/Mansi/dao/StudentDaoInterface.java
+++ src/com/Mansi/dao/StudentDaoInterface.java
@@ -4,9 +4,9 @@ import com.Mansi.model.Student;
 
 public interface StudentDaoInterface {
 
-	public boolean insertStudent(Student s);
-	public boolean delete(int roll);
-	public boolean update(int roll,String update,int ch,Student s);
-	public void showAllStudent();
-	public boolean showStudentById(int roll);
+    public boolean insertStudent(Student s);
+    public boolean delete(int roll);
+    public boolean update(int roll,String update,int ch,Student s);
+    public void showAllStudent();
+    public boolean showStudentById(int roll);
 }
